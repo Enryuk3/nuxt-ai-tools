201f2a864460df5ff2b88e7982b1505ea292b5e0
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-09-18 22:42:07 -0500

    ✨ add theme toggle

---
+++ app/components/theme-toggle.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const mode = useColorMode()
+
+function toggleTheme() {
+  mode.preference = mode.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <div>
+    <UButton
+      color="neutral"
+      variant="ghost"
+      :icon="mode.value === 'dark' ? 'i-lucide-moon' : 'i-lucide-sun'"
+      @click="toggleTheme"
+    />
+  </div>
+</template>
