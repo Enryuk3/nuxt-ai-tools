fa7790e4d63fc4d72fc3910c59564ee75d36c0e3
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-09-21 04:06:39 -0500

    ✨ add blog title generator page

---
+++ app/pages/dashboard/generate-blog-title.vue
@@ -0,0 +1,115 @@
+<script lang="ts" setup>
+import type { FormSubmitEvent } from '@nuxt/ui'
+import type { FetchError } from 'ofetch'
+import * as z from 'zod'
+
+definePageMeta({
+  layout: 'dashboard',
+})
+
+const schema = z.object({
+  blogTopic: z.string().min(1, 'Article topic is required'),
+  blogCategory: z.string(),
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive<Schema>({
+  blogTopic: '',
+  blogCategory: 'General',
+})
+
+const blogCategories = ref([
+  'General',
+  'Technology',
+  'Business',
+  'Health',
+  'Lifestyle',
+  'Education',
+  'Travel',
+  'Food',
+])
+
+const isLoading = ref(false)
+const error = ref<AppError | null>(null)
+const content = ref('')
+
+async function generateArticle(event: FormSubmitEvent<Schema>) {
+  try {
+    isLoading.value = true
+
+    const data = await $fetch('/api/ai-tools/generate-blog-title', {
+      method: 'POST',
+      body: {
+        blogTopic: event.data.blogTopic,
+        blogCategory: event.data.blogCategory,
+      },
+    })
+
+    if (data) {
+      content.value = data
+      state.blogTopic = ''
+      state.blogCategory = 'General'
+    }
+  }
+  catch (e) {
+    const err = e as FetchError
+    error.value = getError(err)
+  }
+  finally {
+    isLoading.value = false
+  }
+}
+</script>
+
+<template>
+  <div>
+    <UContainer class="h-[calc(100dvh-80px)] flex flex-col py-4">
+      <PageHeading
+        title="Blog Title Generator"
+        description="Discover compelling and attention-grabbing blog titles instantly with AI-powered suggestions"
+      />
+
+      <UAlert
+        v-if="error"
+        color="error"
+        variant="subtle"
+        :title="`${error?.statusCode} `"
+        :description="error?.statusMessage"
+        class="mb-2"
+      />
+      <div class="py-4 w-full max-w-full">
+        <div class="flex flex-col space-y-2">
+          <div>
+            <UCard>
+              <UForm
+                :schema="schema"
+                :state="state"
+                class="space-y-4"
+                @submit="generateArticle"
+              >
+                <UFormField name="blogTopic" label="Blog Topic">
+                  <UInput v-model="state.blogTopic" class="w-full" />
+                </UFormField>
+                <UFormField name="blogCategory" label="Blog Category">
+                  <USelect v-model="state.blogCategory" :items="blogCategories" class="w-full" />
+                </UFormField>
+                <UButton type="submit" color="neutral">
+                  Submit
+                </UButton>
+              </UForm>
+            </UCard>
+          </div>
+          <UCard :ui="{ body: 'h-[calc(100dvh-350px)]' }" class="flex-1 overflow-y-auto">
+            <div v-if="content" class="h-full">
+              <MDC :value="content" />
+            </div>
+            <div v-else-if="isLoading && !content" class="flex flex-col justify-center items-center">
+              Generating Blogs title...
+            </div>
+          </UCard>
+        </div>
+      </div>
+    </UContainer>
+  </div>
+</template>
---
+++ server/api/ai-tools/generate-blog-title.post.ts
@@ -0,0 +1,34 @@
+import { openai } from '~~/server/utils/openai'
+
+export default defineEventHandler(async (event) => {
+  const { blogTopic, blogCategory } = await readBody(event)
+
+  if (!blogTopic) {
+    throw createError({
+      statusCode: 4000,
+      statusMessage: 'Blog topic is required',
+    })
+  }
+  if (!blogCategory) {
+    throw createError({
+      statusCode: 4000,
+      statusMessage: 'Blog category is required',
+    })
+  }
+
+  const prompt = `Generate a blog title for keywords ${blogTopic} in the category ${blogCategory}`
+
+  const res = await openai.chat.completions.create({
+    model: 'gemini-2.0-flash',
+    messages: [
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.5,
+    max_completion_tokens: 200,
+  })
+
+  return res.choices[0].message.content
+})
