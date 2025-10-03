2043b6c93436449e3abefed1943c620637f45ac5
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-10-02 23:36:01 -0500

    ✨ add get session to composable

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,8 +1,11 @@
+import type { ClientOptions, InferSessionFromClient, InferUserFromClient } from 'better-auth'
 import type { RouteLocationRaw } from 'vue-router'
 import { polarClient } from '@polar-sh/better-auth'
 import { createAuthClient } from 'better-auth/vue'
 
 export function useAuth() {
+  const headers = import.meta.server ? useRequestHeaders() : undefined
+
   const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: 'http://localhost:3000',
@@ -10,6 +13,37 @@ export function useAuth() {
       polarClient(),
     ],
   })
+
+  const session = useState<InferSessionFromClient<ClientOptions> | null>('auth:session', () => null)
+  const user = useState<InferUserFromClient<ClientOptions> | null>('auth:user', () => null)
+  const sessionFetching = import.meta.server ? ref(false) : useState('auth:session-fetching', () => false)
+
+  const fetchSession = async () => {
+    if (sessionFetching.value) {
+      return
+    }
+
+    sessionFetching.value = true
+
+    const { data } = await authClient.getSession({
+      fetchOptions: {
+        headers,
+      },
+    })
+
+    session.value = data?.session || null
+    user.value = data?.user || null
+    sessionFetching.value = false
+    return data
+  }
+
+  if (import.meta.client) {
+    authClient.$store.listen('$sessionSignal', async (signal) => {
+      if (!signal)
+        return
+      await fetchSession()
+    })
+  }
 
   const logout = async ({ redirectTo }: { redirectTo?: RouteLocationRaw } = {}) => {
     await authClient.signOut()
@@ -29,5 +63,9 @@ export function useAuth() {
     signUp: authClient.signUp,
     logout,
     upgradeUserToPro,
+    session,
+    user,
+    loggedIn: computed(() => !!session.value),
+    fetchSession,
   }
 }
